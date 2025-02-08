79f189f1c9472ded3eae555eb045a3393342ce07
jackiewaang <zjackiewang03@gmail.com>
2025-02-08 20:14:39 +0000

    changed primary and secondary colours and added start simulation button

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -4,15 +4,26 @@ const Sidebar = () => {
 
   const directions = ["North", "East", "South", "West"]
 
+  const validateData = () => {
+
+  }
+
   return (
-    <div className="bg-neutral-300 flex flex-col overflow-y-auto">
+    <div className="bg-background flex flex-col overflow-y-auto">
       <div className="mt-5 ml-5">
-        <button type="button" className="bg-blue-500 text-white px-8 py-5 rounded-2xl shadow-md text-2xl hover:bg-blue-400 transition duration-300 cursor-pointer">History</button>
+        <button type="button" className="bg-secondary px-8 py-5 rounded-2xl shadow-md text-2xl hover:bg-blue-300 transition duration-300 cursor-pointer">History</button>
       </div>
       <div className="p-5 flex flex-col gap-5">
         {directions.map((direction, index) => (
           <InputMenu key={index} inbound={direction} />
         ))}
+        <div className="flex justify-center">
+          <button onClick={validateData} className="text-2xl rounded-lg text-white bg-primary border px-5 py-3 shadow-md cursor-pointer hover:bg-accent transition duration-300" type="button">Start Simulation</button>
+        </div>
+        
+      </div>
+      <div>
+        
       </div>
     </div>
   )
