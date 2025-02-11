f6447fc954205ba4db24d1891cead73aa9392854
ahmadmuqry <ahmadmuqry@gmail.com>
2025-02-11 18:21:52 +0000

    Added an endpoint to get csrf token.

--- backend/simulation/urls.py
+++ backend/simulation/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import create_simulation, check_simulation_status, start_simulation, get_completed_simulations, delete_simulation
+from .views import create_simulation, check_simulation_status, start_simulation, get_completed_simulations, delete_simulation, get_csrf_token
 
 urlpatterns = [
+    path('get-csrf-token/', get_csrf_token),
     path('create-simulation/', create_simulation),
     path('start-simulation/', start_simulation),
     path('check-simulation-status/', check_simulation_status),
