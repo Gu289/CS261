b293510567557f21a48103649e341880ac6b9cdb
ahmadmuqry <ahmadmuqry@gmail.com>
2025-02-23 21:11:45 +0000

    Add error handling to simulation execution; update simulation status on failure

--- backend/simulation/tasks.py
+++ backend/simulation/tasks.py
@@ -19,18 +19,18 @@ def run_simulation(simulation_id):
         raise Exception("Simulation not found")
     
     # Create an instance of the simulation engine
-    engine = SimulationEngine(simulation)
-    
-    # Run the simulation
-    results = engine.start()
-
-    # Update simulation object in the database.
-    # simulation.avg_wait_time = results.average_wait_time
-    # simulation.max_wait_time = results.max_wait_time
-    # simulation.max_queue_length = results.max_queue_length
-    simulation.metrics = results
-    simulation.simulation_status = "completed"
-    simulation.save()
-    
-    print(results)
+    try:
+        engine = SimulationEngine(simulation)
+        
+        # Run the simulation
+        results = engine.start()
+        simulation.metrics = results
+        simulation.simulation_status = "completed"
+        simulation.save()
+        
+        print(results)
+    except Exception as e:
+        print(e)
+        simulation.simulation_status = "failed"
+        simulation.save()
 
