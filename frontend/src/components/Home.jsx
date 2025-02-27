8ba50f0196b38681c46c35b94d42ef98e8fc619c
AbdullahKhalid786 <solvayonceagain@gmail.com>
2025-02-27 14:20:33 +0000

    added popup to display metrics after simulation finsihed

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,28 +1,3 @@
-// import Sidebar from "./ui/Sidebar"
-// import Simulation from "./simulation/Simulation"
-// import { useState } from "react"
-
-// const Home = ( { handleSimId }) => {
-
-//     const [showResults, setShowResults] = useState(false)
-
-//     const handleResults = (data) => {
-//         setShowResults(!showResults);
-//         console.log(data)
-//     }
-
-//     return(
-//         <div className="bg-white h-screen grid grid-cols-3 overflow-y-hidden">
-//             <Sidebar handleSimId={handleSimId} handleResults={handleResults}/>
-//             <Simulation />
-//             <dialog>
-//                 C
-//             </dialog>
-//         </div>
-//     )
-// }
-
-// export default Home
 
 
 import React, { useState, useEffect } from "react";
